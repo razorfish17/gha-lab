90c0601da7c96e63a1ad42ccd3dd035700c05c55
Will Lamb <w.lamb@ed.ac.uk>
2023-11-16 12:39:23 +0000

    Added the two files  now-

---
+++ functions.py
@@ -0,0 +1,5 @@
+def count10():
+    return [i for i in range(10)]
+
+def divideby2(my_int):
+    return my_int/2
---
+++ test/test_functions.py
@@ -0,0 +1,13 @@
+"""
+unit testing for functions.py
+"""
+import functions
+
+def testCounto10():
+    assert sum(count10) == 45
+    assert len(count10) == 10
+
+def testDivideby2():
+    assert divideby2(10) == 5
+
+
